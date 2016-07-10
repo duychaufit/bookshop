75c509fd52cd9f16132819890751f74c0bb561fc
Hoang Duy <duychaufit@gmail.com>
2016-07-10 06:02:00 +0000

    change profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,10 @@ class ApplicationController < ActionController::Base
   end
   
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :name
-      devise_parameter_sanitizer.for(:account_update) << :name
-    end
     
     def configure_sign_up_params
-      # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
     
     
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,42 +1,42 @@
 class CategoriesController < ApplicationController
 
-  # def new
-  #   @page_title = 'Add a new Category'
-  #   @category = Category.new
-  # end
+  def new
+    @page_title = 'Add a new Category'
+    @category = Category.new
+  end
 
-  # def create
-  #   @category = Category.new(category_params)
-  #   if @category.save
-  #     flash[:notice] = "Category created."
-  #     redirect_to categories_path
-  #   else
-  #     render 'new'
-  #   end
-  # end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created."
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
 
-  # def update
-  # end
+  def update
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def destroy
-  # end
+  def destroy
+  end
 
-  # def index
-  #   @categories = Category.all
-  # end
+  def index
+    @categories = Category.all
+  end
 
-  # def show
-  #   @category = Category.find params[:id]
-  #   @books = @category.books
-  #   @categories = Category.all
-  # end
+  def show
+    @category = Category.find params[:id]
+    @books = @category.books
+    @categories = Category.all
+  end
   
-  # private
-  #     def category_params
-  #       params.require(:category).permit(:name)
-  #     end
+  private
+      def category_params
+        params.require(:category).permit(:name)
+      end
   
   end
